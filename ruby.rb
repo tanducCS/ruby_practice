716d63fd2152e565a04fc4930b3278a8e349b8f1
tanducCS <hotanduc.cs@gmail.com>
2023-06-22 16:39:14 +0700

    first commit

---
+++ ruby.rb
@@ -0,0 +1,47 @@
+class Person
+  #   Constructor
+  @@count = 0
+
+  def initialize (name, age)
+    @@count += 1
+    @name = name
+    @age = age
+  end
+
+  # Getter
+  def getName
+    @name
+  end
+
+  def getAge
+    @age
+  end
+
+  #   Setter
+  def setName=(name)
+    @name = name
+  end
+
+  def setAge=(age)
+    @age = age
+  end
+
+  def introduce
+    puts "My name is #{@name}. I'm #{@age} years old"
+  end
+
+  def self.totalcount
+    puts "Total number of people is #{@@count}"
+  end
+end
+
+people1 = Person.new('Bao', '18')
+people2 = Person.new('Duc', '30')
+
+puts Person.totalcount
+
+array = []
+age = Random.new
+20.times { |i| array.push(Person.new("Person #{i}", age.rand(1..90))) }
+puts array
+
